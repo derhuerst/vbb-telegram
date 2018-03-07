3fb57b27f755c089747a42b6c7af1a63648e0ef6
Jannis R <mail@jannisr.de>
2018-03-07 17:51:04 +0100

    wip: logging, command parsing

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict'
 
-// todo
+const Bot = require('telegraf')
+
+const logging = require('./lib/logging')
+const storage = require('./lib/storage')
+const command = require('./lib/command')
+
+const TOKEN = process.env.TOKEN
+if (!TOKEN) {
+	console.error('Missing TOKEN env var.')
+	process.exit(1)
+}
+
+const bot = new Bot(TOKEN)
+bot.use(logging)
+bot.use(command)
+
+bot.startPolling() // todo: web hook
